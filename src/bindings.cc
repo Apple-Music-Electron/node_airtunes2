ab0e2cd841d521ce4d5907c2c3e3b0075bb49266
vapormusic <vietanhfat@gmail.com>
2021-10-20 19:30:53 +0700

    Update bindings.cc

--- src/bindings.cc
+++ src/bindings.cc
@@ -1,20 +1,22 @@
 #include <v8.h>
 #include <node.h>
+#include "nan.h"
 
 using namespace v8;
 using namespace node;
 
+
 namespace nodeairtunes {
 
-void InitCodec(Handle<Object>);
+void InitCodec(Global<Object>);
 #ifdef __APPLE__
-void InitCoreAudio(Handle<Object>);
+void InitCoreAudio(Global<Object>);
 #endif
 
-void Initialize(Handle<Object> target) {
+void Initialize(Global<Object> target) {
 
   Isolate* isolate = v8::Isolate::GetCurrent();
-  HandleScope scope(isolate);
+  Nan::HandleScope scope(isolate);
 
   InitCodec(target);
 #ifdef __APPLE__
