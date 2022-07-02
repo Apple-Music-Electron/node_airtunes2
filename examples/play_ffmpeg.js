3fc151fc4e43aa07d0f0236d83f631bb0a52526b
vapormusic <vietanhfat@gmail.com>
2022-07-02 09:50:14 +0700

    prioritze native alac encode

--- examples/play_ffmpeg.js
+++ examples/play_ffmpeg.js
@@ -1,10 +1,10 @@
 var AirTunes = require('../lib/'),
     spawn = require('child_process').spawn,
     argv = require('optimist')
-      .usage('Usage: $0 --host [host] --port [num] --ffmpeg [path] --file [path] --volume [num] --password [string] --mode [mode]')
+      .usage('Usage: $0 --host [host] --port [num] --ffmpeg [path] --file [path] --volume [num] --password [string] --mode [mode] --debug [mode]')
       .default('port', 5002)
       .default('volume', 50)
-      .default('ffmpeg', 'C:\\ffmpeg-4.4-essentials_build\\bin\\ffmpeg.exe')
+      .default('ffmpeg', 'E:\\ffmpeg-20180122-2e96f52-win64-shared\\bin\\ffmpeg.exe')
       .default('file', './wakeup.mp3')
       .demand(['host'])
       .argv;
@@ -12,11 +12,11 @@ var AirTunes = require('../lib/'),
 console.log('adding device: ' + argv.host + ':' + argv.port);
 var airtunes = new AirTunes();
 argv.txt = [
-    'cn=1,2,3',
+    'cn=0,1,2,3',
     'da=true',
     'et=0,3,5',
-    'ft=0x4A7FCA00,0xBC354BD0',
-    'sf=0x98484',
+    "ft=0x4A7FCA00,0xBC354BD0",
+    "sf=0x98404",
     'md=0,1,2',
     'am=AudioAccessory5,1',
     'pk=lolno',
