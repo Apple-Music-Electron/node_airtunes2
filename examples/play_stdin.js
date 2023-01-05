670cf1598e9a84e64515912f409b357076f4d50e
vapormusic <vietanhfat@gmail.com>
2023-01-05 17:42:31 +0700

    hmm

--- examples/play_stdin.js
+++ examples/play_stdin.js
@@ -31,19 +31,99 @@ console.log('example: type sample.pcm | node play_stdin.js --host <AirTunes host
 
 // Only works on OSX
 // airtunes.addCoreAudio();
-
+process.env.UV_THREADPOOL_SIZE = 6;
 console.log('adding device: ' + argv.host + ':' + argv.port);
 var airtunes = new AirTunes();
 var device = airtunes.add(argv.host, argv);
+var wsConnection = null;
+var { WebSocketServer } = require('ws');
+const wss = new WebSocketServer({ port: 8980 });
+wss.on('connection', function connection(ws) {
+  wsConnection = ws;
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    parsed_data = JSON.parse(data);
+    if (parsed_data.type == "addDevices") {
+      // Sample data for adding devices:
+      //'{"type":"addDevices",
+      // "host":"192.168.3.4",
+      // "args":{"port":7000,
+      // "volume":50,
+      // "password":3000,
+      // "txt":["tp=UDP","sm=false","sv=false","ek=1","et=0,1","md=0,1,2","cn=0,1","ch=2","ss=16","sr=44100","pw=false","vn=3","txtvers=1"],
+      // "airplay2":1,
+      // "debug":true,
+      // "forceAlac":false}}'
+      airtunes.add(parsed_data.host, parsed_data.args);
+    } else if (parsed_data.type == "setVolume"){
+        // Sample data for setting volume:
+        // {"type":"setVolume",
+        //  "devicekey": "192.168.3.4:7000",
+        //  "volume":30}
+        airtunes.setVolume(parsed_data.devicekey, parsed_data.volume,null);
+    } else if (parse.type == "setProgress"){
+        // Sample data for setting progress:
+        // {"type":"setProgress",
+        //  "devicekey": "192.168.3.4:7000",
+        //  "progress": 0,
+        //  "duration": 0}
+        airtunes.setProgress(parsed_data.devicekey, parsed_data.progress, parsed_data.duration,null);
+    } else if (parse.type == "setArtwork"){
+        // Sample data for setting artwork:
+        // {"type":"setArtwork",
+        //  "devicekey": "192.168.3.4:7000",
+        //  "contentType" : "image/png";
+        //  "artwork": "hex data"}
+        airtunes.setArtwork(parsed_data.devicekey, Buffer.from(parsed_data.artwork,"hex"),null);
+    } else if (parse.type == "setTrackInfo"){
+        // Sample data for setting track info:
+        // {"type":"setTrackInfo",
+        //  "devicekey": "192.168.3.4:7000",
+        //  "artist": "John Doe",
+        //  "album": "John Doe Album",
+        //  "name": "John Doe Song"}
+        airtunes.setTrackInfo(parsed_data.devicekey, parsed_data.name, parsed_data.artist, parsed_data.album, parsed_data.name,null);
+    } else if (parse.type == "setPasscode"){
+        // Sample data for setting passcode:
+        // {"type":"setPasscode",
+        //  "devicekey": "192.168.3.4:7000",
+        //  "passcode": "1234"}
+        airtunes.setPasscode(parsed_data.devicekey, parsed_data.passcode);
+    } else if (parse.type == "stop"){
+        // Sample data for stopping:
+        // {"type":"stop",
+        //  "devicekey": "192.168.3.4:7000"}
+        airtunes.stop(parsed_data.devicekey);
+    } else if (parse.type == "stopAll"){
+        // Sample data for stopping all:
+        // {"type":"stopAll"}
+        airtunes.stopAll();
+    }
+  });
+});    
 
+airtunes.on('device', function(key, status, desc) {
+  let status_json =  {
+    type : "deviceStatus",
+    key : key,
+    status: status,
+    desc: desc
+  }
+  if(wsConnection != null){
+   
+  wsConnection.send(JSON.stringify(status_json));
+  }
+  console.log("deviceStatus", key, status, desc);
+  process.stdout.write(JSON.stringify(status_json))
+})
 
 // when the device is online, spawn ffmpeg to transcode the file
 device.on('status', function(status) {
   console.log('status: ' + status);
 
-  if(status === 'need_password'){
-    device.setPasscode(argv.password);
-  }
+  // if(status === 'need_password'){
+  //   device.setPasscode(argv.password);
+  // }
 
   if(status !== 'ready')
     return;
@@ -51,9 +131,77 @@ device.on('status', function(status) {
   if(status == 'ready') {
   }
 
-
+  process.stdin.on('data', function (data) {
+    let commandbytes = Buffer.from([0x61, 0x74, 0x63, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x63, 0x3d, 0x70, 0x6c, 0x61, 0x79, 0x0a]);
+    //check if data starts with commandbytes
+    if (data.slice(0,15).equals(commandbytes)){
+      console.log("commandbytes found");
+      let command = data.slice(15).toString();
+      console.log(command);
+      parsed_data = JSON.parse(command);
+      if (parsed_data.type == "addDevices") {
+        // Sample data for adding devices:
+        //'{"type":"addDevices",
+        // "host":"192.168.3.4",
+        // "args":{"port":7000,
+        // "volume":50,
+        // "password":3000,
+        // "txt":["tp=UDP","sm=false","sv=false","ek=1","et=0,1","md=0,1,2","cn=0,1","ch=2","ss=16","sr=44100","pw=false","vn=3","txtvers=1"],
+        // "airplay2":1,
+        // "debug":true,
+        // "forceAlac":false}}'
+        airtunes.add(parsed_data.host, parsed_data.args);
+      } else if (parsed_data.type == "setVolume"){
+          // Sample data for setting volume:
+          // {"type":"setVolume",
+          //  "devicekey": "192.168.3.4:7000",
+          //  "volume":30}
+          airtunes.setVolume(parsed_data.devicekey, parsed_data.volume,null);
+      } else if (parse.type == "setProgress"){
+          // Sample data for setting progress:
+          // {"type":"setProgress",
+          //  "devicekey": "192.168.3.4:7000",
+          //  "progress": 0,
+          //  "duration": 0}
+          airtunes.setProgress(parsed_data.devicekey, parsed_data.progress, parsed_data.duration,null);
+      } else if (parse.type == "setArtwork"){
+          // Sample data for setting artwork:
+          // {"type":"setArtwork",
+          //  "devicekey": "192.168.3.4:7000",
+          //  "contentType" : "image/png";
+          //  "artwork": "hex data"}
+          airtunes.setArtwork(parsed_data.devicekey, Buffer.from(parsed_data.artwork,"hex"),null);
+      } else if (parse.type == "setTrackInfo"){
+          // Sample data for setting track info:
+          // {"type":"setTrackInfo",
+          //  "devicekey": "192.168.3.4:7000",
+          //  "artist": "John Doe",
+          //  "album": "John Doe Album",
+          //  "name": "John Doe Song"}
+          airtunes.setTrackInfo(parsed_data.devicekey, parsed_data.name, parsed_data.artist, parsed_data.album, parsed_data.name,null);
+      } else if (parse.type == "setPasscode"){
+          // Sample data for setting passcode:
+          // {"type":"setPasscode",
+          //  "devicekey": "192.168.3.4:7000",
+          //  "passcode": "1234"}
+          airtunes.setPasscode(parsed_data.devicekey, parsed_data.passcode);
+      } else if (parse.type == "stop"){
+          // Sample data for stopping:
+          // {"type":"stop",
+          //  "devicekey": "192.168.3.4:7000"}
+          airtunes.stop(parsed_data.devicekey);
+      } else if (parse.type == "stopAll"){
+          // Sample data for stopping all:
+          // {"type":"stopAll"}
+          airtunes.stopAll();
+      }
+    } else {airtunes.write(data);}
+    
+    // process.stdin.pipe(airtunes);
+    // process.stdin.resume();
+  });
   // pipe data to AirTunes
-  process.stdin.pipe(airtunes);
+  //process.stdin.pipe(airtunes);
 });
 
 // monitor buffer events
