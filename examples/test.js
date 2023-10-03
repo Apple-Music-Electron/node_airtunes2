33ffe7bce0b551b41bf868a902d9aedcd7561dd4
vapormusic <vietanhfat@gmail.com>
2023-10-03 22:26:59 +0700

    refactor

--- examples/test.js
+++ examples/test.js
@@ -1,7 +1,8 @@
-const DACPServer = require ('./lib.js');
+const AirTunes = require ('../lib');
 var { WebSocket, WebSocketServer } = require('ws');
 var ab2str = require('arraybuffer-to-string')
-let dacp = new DACPServer();
+let airtunes = new AirTunes();
+let dacp = airtunes.DACPServer;
 let devices = [];
 
 const wss = new WebSocketServer({ port: 9854 });
